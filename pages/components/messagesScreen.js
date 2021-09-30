7893f935df2aa0c8d650f1c377d76fc2c0f64bcc
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2021-09-30 18:57:34 +0300

    dd

--- pages/components/messagesScreen.js
+++ pages/components/messagesScreen.js
@@ -15,7 +15,49 @@ const MessagesScreen = () => {
 
     return (
         <>  
+        <div className="day">
+        <div className="day__row day__header">
+                <div className="day__left">Сегодня</div>
+                <div className="day__right">
+                    <div className="day__sum">-520 ₽</div>
+                </div>
+            </div>
             
+            <div className="day__row">
+                <div className="day__left">Продукты</div>
+                <div className="day__right">
+                    <div className="day__sum">-520 ₽</div>
+                </div>
+            </div>
+            
+            <div className="day__row day__incoming">
+                <div className="day__left">
+                    Продукты 
+                    <div className="day__users">
+                        <div className="day__user"/>
+                        <div className="day__user"/>
+                        <div className="day__user"/>
+                    </div>
+                </div>
+                <div className="day__right">
+                    <div className="day__sum">-520 ₽</div>
+                </div>
+            </div>
+            
+            <div className="day__row">
+                <div className="day__left">dsfkjsd</div>
+                <div className="day__right">
+                    <div className="day__varsum">
+                        <div className="day__varsumline">
+                            <div className="day__sum var">-520 ₽</div>
+                            <div className="day__arrowright"/>
+                            <div className="day__sum">-520 ₽</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="day__scroll"/>
+            </div>
+        </div>
         </>
     )
 }
