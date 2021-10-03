d92774f0a88a0971f7247e74e4eb3c1fd18bf91c
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2021-10-03 17:43:56 +0300

    44

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link href="/fav76.png" rel="apple-touch-icon"/>
+        <link href="/fav76.png" rel="apple-touch-icon" sizes="76x76"/>
+        <link href="/fav120.png" rel="apple-touch-icon" sizes="120x120"/>
+        <link href="/fav152.png" rel="apple-touch-icon" sizes="152x152"/>
       </Head>
       <div className="form">
         
